bad7116bb9ee8c6fa39de5fdc807eab921642f67
JaineDoerzbacher <doerzbacherjaine@gmail.com>
2021-01-20 15:36:14 -0300

    Criação da entidade Planet

---
+++ src/main/kotlin/com/planets/features/entities/Planet.kt
@@ -0,0 +1,16 @@
+package com.planets.features.entities
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "PLANETS")
+data class Planet(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    val nome: String,
+    val solo: String,
+    val descricao: String
+
+)
