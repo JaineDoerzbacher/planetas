fa122195ce983fc2a60ceb6129bb84f483401fa9
JaineDoerzbacher <doerzbacherjaine@gmail.com>
2021-01-20 15:37:54 -0300

    Criação do DTO

---
+++ src/main/kotlin/com/planets/features/dtos/PlanetDTO.kt
@@ -0,0 +1,13 @@
+package com.planets.features.dtos
+
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+data class PlanetDTO(
+
+    val nome: String,
+    val solo: String,
+    val descricao: String
+
+)
